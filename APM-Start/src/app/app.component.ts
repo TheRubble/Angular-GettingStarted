ddce5827f037103b61390b7351dfa8208151f82d
Chriss Barnard <chrissbarnard@Chrisss-MacBook-Pro.local>
2021-12-19 13:37:39 +0000

    Starting to put the product list together

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: "./app.component.html",
+  selector: "pm-root"
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle : string = "Acme Product Management";
 }
